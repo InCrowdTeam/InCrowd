5ac373851561a6dd665a67050a0951169d83f2ac
Giacomo Berti <bertii.giacomo@gmail.com>
2025-06-04 13:11:20 +0200

    Fix schema names and address model typo

--- backend/src/models/Ente.ts
+++ backend/src/models/Ente.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { allegatoSchema } from "./Allegato";
 import { credenzialiSchema } from "./Credenziali";
 
-const userSchema = new mongoose.Schema({
+const enteSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   codiceFiscale: { type: String, required: true},
   biografia: { type: String, required: true},
@@ -10,4 +10,4 @@ const userSchema = new mongoose.Schema({
   credenziali: {type: credenzialiSchema, required: true}
 }, { timestamps: true });
 
-export default mongoose.model("User", userSchema, "utenti");
+export default mongoose.model("Ente", enteSchema, "enti");
